8eff667ebd44e41373d5b2763c768ca599e6e537
Boluwatife2904 <sanvicola2000@gmail.com>
2022-08-15 07:27:25 +0100

    chore: updated og meta tags for the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,12 @@ export default defineNuxtConfig({
             meta: [
                 { name: "theme-color", content: "#02050e" },
                 { name: "viewport", content: "width=device-width, initial-scale=1" },
+                { name: "og:title", content: "Panda NFTs" },
+                { name: "og:site_name", content: "Panda NFTs" },
+                { name: "og:url", content: "https://pandanfts.vercel.app" },
+                { name: "og:description", content: "The world's first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital items." },
+                { name: "og:type", content: "website" },
+                { name: "og:image", content: "https://res.cloudinary.com/juwon-tech/image/upload/v1660544631/Screenshot_2022-08-15_at_07.20.29_ukcy9r.png" },
             ],
             link: [
                 {
