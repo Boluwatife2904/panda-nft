9ea6740866bd01ed566bfc826199e6abe6c6c598
Boluwatife2904 <sanvicola2000@gmail.com>
2022-12-08 15:06:42 +0100

    chore: optimized image used in website maintenance component

--- components/WebsiteInMaintenance.vue
+++ components/WebsiteInMaintenance.vue
@@ -2,8 +2,8 @@
 
 <template>
     <div class="website-maintenance mx-auto text-center">
-        <img src="/images/website-maintenance.svg" alt="Website in Maintenance Image" />
-        <h3 class="heading-3-bold text-white">This page is not yet available.</h3>
+        <nuxt-img format="webp" fit="cover" height="350" width="350" src="/images/website-maintenance.svg" alt="Website in Maintenance Image" />
+        <h3 class="heading-4-bold text-white">This page is not yet available.</h3>
         <p class="paragraph-regular-body text-white">Oooooppsss!!!! I see You've gotten here but I've not started implementing the UI for this page and thus why I have this screen. Please check back later. Thanks 🙌🏽.</p>
     </div>
 </template>
@@ -16,6 +16,11 @@
 
     @media screen and (min-width: 768px) {
         padding: 0 3.2rem;
+    }
+
+    img {
+        height: 100%;
+        max-width: 100%;
     }
 
     h3 {
